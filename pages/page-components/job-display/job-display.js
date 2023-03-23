f8c1d443d4b1f29ebd59892d996d7268051f5e5d
unknown <unknown@example.com>
2023-03-23 11:24:54 +0100

    job list search bar

---
+++ pages/page-components/job-display/job-display.js
@@ -0,0 +1,23 @@
+// pages/page-components/job-display/job-display.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
