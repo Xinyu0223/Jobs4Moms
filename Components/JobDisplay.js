374796d0a47e5bcc2c0e3ac1b7dba35bd540c7a5
Yiyan <yiyan0617@gmail.com>
2023-03-29 23:10:48 -0700

    Add JobDisplay Component

---
+++ Components/JobDisplay.js
@@ -0,0 +1,45 @@
+// Components/JobDisplay.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    companyTitle: {
+      type: String
+    },
+    brief: {
+      type: String
+    },
+    jobTitle: {
+      type: String
+    },
+    location: {
+      type: String
+    },
+    salary: {
+      type: String
+    },
+    ifFullTime: {
+      type: Boolean,
+      value: true
+    },
+    ifPartTime: {
+      type: Boolean,
+      value: true
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
